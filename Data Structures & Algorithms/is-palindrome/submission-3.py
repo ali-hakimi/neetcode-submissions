646e56d8863eac4f2ca0b6e8d80f6bbe473b4641
ali-hakimi <alihakimi014@gmail.com>
2026-08-08 22:07:50 -0700

    Update: is-palindrome - submission-3

--- Data Structures & Algorithms/is-palindrome/submission-3.py
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -1,23 +1,13 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        print(s)
-        i = 0
-        j = len(s) - 1
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
 
-        while i < j:
-            while i < j and not self.isalphaNum(s[i]):
-                i += 1
-            while i < j and not self.isalphaNum(s[j]):
-                j -= 1
-            if s[i] != s[j]:
-                return False
-            i += 1
-            j -= 1
-        return True
-    
-    def isalphaNum(self, c):
-                return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9')) 
+        for n in nums:
+            if n - 1 not in numSet:
+                length = 0
+                while n + length in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
         
