1607232677e1cd3771900fce52727663ba4d411f
ali-hakimi <alihakimi014@gmail.com>
2026-08-18 02:35:28 -0700

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def dfs(cur, i):
+            if len(cur) == k:
+                res.append(cur.copy())
+                return
+            for j in range(i, n + 1):
+                cur.append(j)
+                dfs(cur, j + 1)
+                cur.pop()
+
+
+        dfs([], 1)
+        return res
