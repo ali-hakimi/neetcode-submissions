b2ad753facb6c5ef4d20e2fd20d2361356a58b4b
ali-hakimi <alihakimi014@gmail.com>
2026-08-19 00:36:51 -0700

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        one, two = 0, 0
+
+        for i in range(len(nums)):
+            one, two = max(one, nums[i] + two), one
+        return one
