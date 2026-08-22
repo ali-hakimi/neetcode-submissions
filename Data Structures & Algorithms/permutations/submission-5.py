fbba09abcfd93349c634e66f2ef7f30fa0d74529
ali-hakimi <alihakimi014@gmail.com>
2026-08-21 23:51:11 -0700

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(i):
+            if i == len(nums):
+                return [[]]
+            res = []
+            perms = dfs(i + 1)
+            for p in perms:
+                for j in range(len(p) + 1):
+                    copy = p.copy()
+                    copy.insert(j, nums[i])
+                    res.append(copy)
+            return res
+        
+        return dfs(0)
