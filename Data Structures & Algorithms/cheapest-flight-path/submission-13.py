4161267d6dc09845ecd3d584375913b00512e32f
ali-hakimi <alihakimi014@gmail.com>
2026-08-26 12:28:53 -0700

    Add: cheapest-flight-path - submission-13

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-13.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
+        # bell-man ford algorithm
+
+        prices = [float("inf")] * n
+        prices[src] = 0
+
+        while k >= 0:
+            tempPrices = prices.copy()
+            for s, d, p in flights:
+                if tempPrices[s] == float("inf"):
+                    continue
+                if prices[s] + p < tempPrices[d]:
+                    tempPrices[d] = prices[s] + p
+                
+            prices = tempPrices
+            k-=1
+        return prices[dst] if prices[dst] != float("inf") else -1
